2d573d46445e1163a30fa158ae8b3f605434767e
Aldeni123 <99968783+Aldeni123@users.noreply.github.com>
2022-04-16 05:18:27 -0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,43 +1,139 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  final List<int> ColorCodes = <int>[500, 700, 900];
-  var bulan = ["Juni", "Juli", "Agustus"];
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Container(
-            color: Colors.blue,
-            child: ListView.separated(
-              padding: EdgeInsets.all(10),
-              itemCount: ColorCodes.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  height: 100,
-                  padding: EdgeInsets.all(10),
-                  color: Colors.amber[ColorCodes[index]],
-                  child: Text(
-                    bulan[index],
-                    style: TextStyle(
-                      fontSize: 29,
-                      fontStyle: FontStyle.italic,
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "ALDENI MARGA PUTRA",
+            style: new TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.red,
+          centerTitle: true,
+        ),
+        //backgroundColor: Colors.blue[100],
+        body: Container(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: GridView.count(
+              crossAxisCount: 2,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                        color: Colors.pink),
+                    child: Center(
+                      child: Text(
+                        "pink",
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
                     ),
                   ),
-                );
-              },
-              separatorBuilder: (BuildContext conrext, int index) {
-                return Divider();
-              },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                        color: Colors.green),
+                    child: Center(
+                      child: Text(
+                        "green",
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                        color: Colors.purple),
+                    child: Center(
+                      child: Text(
+                        "purple",
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                        color: Colors.yellow),
+                    child: Center(
+                      child: Text(
+                        "yellow",
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                        color: Colors.brown),
+                    child: Center(
+                      child: Text(
+                        "brown",
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                        color: Colors.grey),
+                    child: Center(
+                      child: Text(
+                        "grey",
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
